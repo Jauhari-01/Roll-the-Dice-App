422bca0ec75fc36ee552f61e253be87fad5321c9
Shubham_Jauhari <shubham.jauhari009@gmail.com>
2022-04-12 00:09:10 +0530

    Basic logic added for the add

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+
+  rollDice() {
+    random = Math.floor(Math.random() * 6) + 1;
+    console.log(random);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity
+          onPress={this.rollDice()}
+          style={styles.button}
+        >
+          <Text
+           style={{color:'white'}}>Roll Dice</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +30,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button:{
+    backgroundColor:'#c686f0',
+    padding:20,
+    margin:10,
+    color:'white',
+    borderRadius:10
+  }
 });
