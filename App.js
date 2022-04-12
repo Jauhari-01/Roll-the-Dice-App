11ffb2ecbd0b6da6ecbccaad0b2f68d01dae17dc
Shubham_Jauhari <shubham.jauhari009@gmail.com>
2022-04-12 23:04:11 +0530

    The code is completed and App is Ready

--- App.js
+++ App.js
@@ -1,40 +1,65 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 
-export default class App extends Component {
 
-  rollDice() {
-    random = Math.floor(Math.random() * 6) + 1;
+const diceArr = {
+  1: require('./assets/D1.png'),
+  2: require('./assets/D2.png'),
+  3: require('./assets/D3.png'),
+  4: require('./assets/D4.png'),
+  5: require('./assets/D5.png'),
+  6: require('./assets/D6.png')
+}
+
+export default function App() {
+
+  //states
+  const [dice, setDice] = useState('1');
+
+
+  function rollDice() {
+    const random = (Math.floor(Math.random() * 6) + 1).toString();
     console.log(random);
+    setDice(random);
   }
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <TouchableOpacity
-          onPress={this.rollDice()}
-          style={styles.button}
-        >
-          <Text
-           style={{color:'white'}}>Roll Dice</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
+  return (
+    <View style={styles.container}>
+      <Image
+        source={diceArr[dice]}
+        style={styles.img}
+      />
+      <TouchableOpacity
+        //onPress takes a function as a argument 
+        onPress={() => {
+          rollDice()
+        }
+        }
+        style={styles.button}
+      >
+        <Text
+          style={{ color: 'white' }}>Roll The Dice</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F6FBB6',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  button:{
-    backgroundColor:'#c686f0',
-    padding:20,
-    margin:10,
-    color:'white',
-    borderRadius:10
+  button: {
+    backgroundColor: '#c686f0',
+    padding: 20,
+    margin: 40,
+    color: 'white',
+    borderRadius: 10
+  },
+  img: {
+    width: 150,
+    height: 150
   }
 });
